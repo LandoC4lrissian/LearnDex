b0b8506b8281f02a7399f74cbcd2cb55d44329a4
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-08-22 16:38:39 +0300

    rainbowkit & educhain added

---
+++ frontend/learn-dex/app/utils/config.ts
@@ -0,0 +1,26 @@
+import { http, createConfig } from "wagmi";
+import { Chain } from "viem";
+
+const educhain: Chain = {
+  id: 656476,
+  name: "EduChain",
+  nativeCurrency: { name: "EduToken", symbol: "EDU", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://rpc.open-campus-codex.gelato.digital"] },
+  },
+  blockExplorers: {
+    default: {
+      name: "EduChain Explorer",
+      url: "https://opencampus-codex.blockscout.com/",
+    },
+  },
+  contracts: {},
+  testnet: true,
+};
+
+export const config = createConfig({
+  chains: [educhain],
+  transports: {
+    [educhain.id]: http("https://rpc.open-campus-codex.gelato.digital"),
+  },
+});
